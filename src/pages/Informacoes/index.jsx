3b7d555b46815dc03dabc393b4c5f0bc94040cfa
= <lucas.freire61@aluno.ifce.edu.br>
2022-11-17 00:22:20 -0300

    commit finished

--- src/pages/Informacoes/index.jsx
+++ src/pages/Informacoes/index.jsx
@@ -5,6 +5,9 @@ import familia from '/src/assets/img/pai-e-filha.png'
 import kids from '/src/assets/img/playtime.png'
 import medico from '/src/assets/img/equipe-medica.png'
 import apoio from '/src/assets/img/assistencia-social.png'
+import arte from '/src/assets/img/arte.jpg'
+import sono from '/src/assets/img/sono.jpg'
+import aula from '/src/assets/img/aula.jpg'
 
 import { Link } from "react-router-dom";
 
@@ -17,10 +20,10 @@ export default function Informacoes() {
     <img className="banner" src={banner} alt="" />
 
     <main className="menu">
-      <h2>Selecione seu Perfil</h2>
+      <h2>Selecione seu <span>Perfil</span></h2>
 
       <div className="card cardInfo">
-        <div className="card-body">
+        <div className="card-body cardBody">
           <h3 className="card-title">Pais, mães e cuidadores</h3>
           <img className="imgCard" src={familia} alt="" />
           <Link href="#" className="card-link">Acessar</Link>
@@ -28,7 +31,7 @@ export default function Informacoes() {
       </div>
 
       <div className="card cardInfo">
-        <div className="card-body">
+        <div className="card-body cardBody">
           <h3 className="card-title">Crianças</h3>
           <img className="imgCard"  src={kids} alt="" />
           <Link href="#" className="card-link">Acessar</Link>
@@ -36,7 +39,7 @@ export default function Informacoes() {
       </div>
 
       <div className="card cardInfo">
-        <div className="card-body">
+        <div className="card-body cardBody">
           <h3 className="card-title">Médicos</h3>
           <img className="imgCard"  src={medico} alt="" />
           <Link href="#" className="card-link">Acessar</Link>
@@ -44,13 +47,52 @@ export default function Informacoes() {
       </div>
 
       <div className="card cardInfo">
-        <div className="card-body">
+        <div className="card-body cardBody">
           <h3 className="card-title">Profissionais de apoio</h3>
           <img className="imgCard"  src={apoio} alt="" />
           <Link href="#" className="card-link">Acessar</Link>
         </div>
       </div>
     </main>
+
+    <section className="blog">
+    <h2 className="titleBlog">Blog</h2>
+      <div className="mainBlog">
+        <div className="card imgBlog">
+        <img src={arte} class="card-img-top" alt="..."/>
+        <div className="card-body">
+          <h5 className="card-title">Arteterapia: quando a arte faz parte do tratamento do autismo</h5>
+          <p className="card-text">Você já ouviu falar sobre a arteterapia? Trata-se de um tratamento que..</p>
+        </div>
+        <div className="card-body">
+          <Link href="#" class="linkBlog">Leia Mais</Link>
+        </div>
+            </div>
+        
+            <div className="card imgBlog">
+        <img src={sono} class="card-img-top" alt="..."/>
+        <div className="card-body">
+          <h5 className="card-title">Autismo e sono: por que muitas crianças e adolescentes têm problemas para dormir?</h5>
+          <p className="card-text">Autismo e sono - ou melhor, autismo e a falta de sono - muitas vezes são..</p>
+        </div>
+        <div className="card-body">
+          <Link href="#" class="linkBlog">Leia Mais</Link>
+        </div>
+            </div>
+        
+        <div className="card imgBlog">
+        <img src={aula} class="card-img-top" alt="..."/>
+        <div className="card-body">
+          <h5 className="card-title">Autismo na educação infantil: 10 ideias para uma sala de aula inclusiva</h5>
+          <p className="card-text">Trazer o autismo para o contexto da sala de aula é, sem dúvida, um..</p>
+        </div>
+        <div className="card-body">
+          <Link href="#" class="linkBlog">Leia Mais</Link>
+        </div>
+            </div>
+      </div>
+
+    </section>
 
 
     <Footer />
